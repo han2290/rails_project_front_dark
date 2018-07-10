72d90bb8d5a7e813c0fd4d3bd91b0e22b805544d
Hanum <cogksdna@naver.com>
2018-07-10 08:40:56 +0000

    f

---
+++ app/assets/javascripts/boards.js
@@ -0,0 +1,17 @@
+//= require detect
+//= require fastclick
+//= require jquery.blockUI
+//= require waves
+//= require jquery.nicescroll
+//= require jquery.slimscroll
+//= require jquery.scrollTo.min
+
+//Index
+//= require sweetalert2.min
+//= require jquery.sweet-alert.init
+
+//Show
+//....
+
+//= require jquery.core
+//= jquery.app
